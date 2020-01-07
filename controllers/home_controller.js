c1b09f2b1814f8461bd3126681c1b15dbb945777
ishan-chaudhary <ishan4220@gmail.com>
2020-01-07 22:23:59 +0530

    links changed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -6,6 +6,21 @@ module.exports.home=(req,res)=>{
 
 module.exports.register=(req,res)=>{
     return res.render('register',{
-        title:'SIH | Home'
+        title:'SIH | Register'
+    });
+}
+module.exports.contact=(req,res)=>{
+    return res.render('contact',{
+        title:'SIH | Contact'
+    });
+}
+module.exports.shoppingCart=(req,res)=>{
+    return res.render('shopping-cart',{
+        title:'SIH | Shopping Cart'
+    });
+}
+module.exports.login=(req,res)=>{
+    return res.render('login',{
+        title:'SIH | Login'
     });
 }
