36485d6ce60ef4b7c3bb90b50311de514f2ad0e5
abhi-el-nino <abhinavgautam36@gmail.com>
2020-01-20 15:04:31 +0530

    removed auth

--- controllers/api/api_home_controller.js
+++ controllers/api/api_home_controller.js
@@ -1,5 +1,6 @@
 const request = require('async-request');
 const Item = require('../../models/item');
+const User=require('../../models/User');
 const jwt = require('jsonwebtoken');
 module.exports.api_home = (req, res) => {
     return res.json(200, {
@@ -28,12 +29,8 @@ module.exports.whetherReport = async (req, res) => {
 }
 module.exports.getAllPRoducts = async (req, res) => {
     try {
-        if (req.user != req.params.id) {
-            return res.json(404,{
-                message: "UNAUTHORIZED"
-            });
-        }
-        let items = await Item.find({ farmer: req.params.id });
+       let farmer=await User.find({emailOrPhone:req.params.id})
+        let items = await Item.find({ farmer: farmer._id });
         return res.json(200,{
             message: "All products for the requsting farmer",
             data: {
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,12 @@
 const express=require('express');
 const router=express.Router();
-const passport=require('passport');
 const homeController=require('../../controllers/api/api_home_controller');
 router.get('/',homeController.api_home);
 router.get('/weather',homeController.whetherReport);
-router.get('/get-all-products/:id',passport.authenticate('jwt',{session:false}),homeController.getAllPRoducts);
+router.get('/get-all-products/:id',homeController.getAllPRoducts);
 router.post('/register',homeController.createUser);
 router.post('/create-sesion',homeController.createSession);
-router.post('/upload-item',passport.authenticate('jwt',{session:false}),homeController.addProduct);
-router.post('/update',passport.authenticate('jwt',{session:false}),homeController.updateProduct);
+router.post('/upload-item',homeController.addProduct);
+router.post('/update',homeController.updateProduct);
 
 module.exports=router;
