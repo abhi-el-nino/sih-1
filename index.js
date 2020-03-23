b007dffc0f2bb7116187504ebd65c5b31ec4d438
ishan chaudhary <ishan4220@gmail.com>
2020-03-23 16:25:53 +0530

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 let bodyParser = require('body-parser');
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 //const db = require('./config/mongoose');
 const path=require('path');
