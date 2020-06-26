f1d8ffe033585eec24bbc13e0af49a6a8e74e382
ishan chaudhary <ishan4220@gmail.com>
2020-06-27 00:22:19 +0530

    payment_check

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -1,46 +1,44 @@
 const Items = require('../models/item');
 const Users = require('../models/User');
 module.exports.upload = (req, res) => {
-    if (req.user._doc.role === "Farmer") {
-        Items.uploadedImage(req, res, async function (err) {
+    Items.uploadedImage(req, res, async function (err) {
 
-            try {
-                if (err) {
-                    console.log("multer error", err);
-                    return;
-                }
-                if (!req.file) {
-                    console.log("image not uploaded");
-                    return;
-                } else {
-                    let newItem = await Items.create({
-                        title: req.body.item_name,
-                        farmer: req.user._id,
-                        price: req.body.price,
-                        quality: 5,
-                        quantity: req.body.quantity
-                    });
-                    newItem.image = `${Items.imagePath}/${req.file.filename}`;
-                    await newItem.save();
-
-                    if (req.xhr) {
-                        console.log("uploading via ajax")
-                        return res.status(200).json({
-                            data: {
-                                item: newItem
-                            },
-                            message: "Item Uploaded!"
-                        });
-
-                    }
-                    return res.redirect('back');
-                }
-
-
-            } catch (error) {
-                console.log(error);
+        try {
+            if (err) {
+                console.log("multer error", err);
                 return;
             }
-        });
-    }
+            if (!req.file) {
+                console.log("image not uploaded");
+                return;
+            } else {
+                let newItem = await Items.create({
+                    title: req.body.item_name,
+                    farmer: req.user._id,
+                    price: req.body.price,
+                    quality: 5,
+                    quantity: req.body.quantity
+                });
+                newItem.image = `${Items.imagePath}/${req.file.filename}`;
+                await newItem.save();
+
+                if (req.xhr) {
+                    console.log("uploading via ajax")
+                    return res.status(200).json({
+                        data: {
+                            item: newItem
+                        },
+                        message: "Item Uploaded!"
+                    });
+
+                }
+                return res.redirect('back');
+            }
+
+
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+    });
 }
