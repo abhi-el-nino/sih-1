e35031b783d06190d92a497c8ad107e2e0102d7d
ishan chaudhary <ishan4220@gmail.com>
2020-06-27 22:34:26 +0530

    changes in farmer model

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,7 @@ const passport = require("passport");
 const Item = require("../../models/item");
 const homeController=require('../../controllers/api/api_home_controller');
 const { ObjectId } = require('mongoose').Types;
+router.use('/mobile',require('./mobile'))
 router.post("/submit-number",homeController.numberVerification);
 router.post("/submit-otp",homeController.submitOtpfromAdmin);
 router.get("/check-session",passport.authenticate('jwt',{session:false}),homeController.checkSession);
